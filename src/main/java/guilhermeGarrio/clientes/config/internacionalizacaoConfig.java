1a8e331771033f87e11bcb3a35e911e570b56077
GuilhermeGarrio <guilhermegarrio@outlook.com>
2023-05-22 22:53:15 -0300

    deleção criada

--- src/main/java/guilhermeGarrio/clientes/config/internacionalizacaoConfig.java
+++ src/main/java/guilhermeGarrio/clientes/config/internacionalizacaoConfig.java
@@ -2,7 +2,6 @@ package guilhermeGarrio.clientes.config;
 
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.support.MessageSourceAccessor;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
